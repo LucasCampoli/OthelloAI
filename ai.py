cf38cd6afc02a490e363f4a7fbc98f8664467e97
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2025-01-28 20:54:56 +0100

    Move heuristic functions to ai

--- ai.py
+++ ai.py
@@ -43,3 +43,28 @@ class AI:
 
     def utility(self, board):
         return 0
+
+    # Count player frontier and opponent frontier
+    def get_frontier_disks(self, board):
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+        player_frontier = 0
+        opponent_frontier = 0
+
+        for i in range(8):
+            for j in range(8):
+                if board[i][j] == self.player_color or board[i][j] == self.opposite_color:
+                    is_frontier = False
+                    for di, dj in directions:
+                        neighbor_i = i + di
+                        neighbor_j = j + dj
+                        if 0 <= neighbor_i < 8 and 0 <= neighbor_j < 8:
+                            if board[neighbor_i][neighbor_j] == 0:
+                                is_frontier = True
+                                break
+
+                    if is_frontier:
+                        if board[i][j] == self.player_color:
+                            player_frontier += 1
+                        else:
+                            opponent_frontier += 1
+        return player_frontier, opponent_frontier
--- game_rules.py
+++ game_rules.py
@@ -20,30 +20,6 @@ def is_valid_play(board, col, row, color):
             c += dc
     return False
 
-# Count player frontier and opponent frontier
-def get_frontier_disks(board, player_color, opponent_color):
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-    player_frontier = 0
-    opponent_frontier = 0
-
-    for i in range(8):
-        for j in range(8):
-            if board[i][j] == player_color or board[i][j] == opponent_color:
-                is_frontier = False
-                for di, dj in directions:
-                    neighbor_i = i + di
-                    neighbor_j = j + dj
-                    if 0 <= neighbor_i < 8 and 0 <= neighbor_j < 8:
-                        if board[neighbor_i][neighbor_j] == 0:
-                            is_frontier = True
-                            break
-
-                if is_frontier:
-                    if board[i][j] == player_color:
-                        player_frontier += 1
-                    else:
-                        opponent_frontier += 1
-    return player_frontier, opponent_frontier
 
 
 def get_valid_plays(board, color):
