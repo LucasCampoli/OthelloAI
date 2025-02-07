913e534733324c32beb8e45e13eb2396f79ccf6b
FRANCO AGUSTÍN GHIGLIANI <fghigliani@itba.edu.ar>
2025-02-07 14:49:31 +0100

    Fixed ai skipping turn and improved heuristics

--- ai.py
+++ ai.py
@@ -1,18 +1,22 @@
 import copy
 
+from numpy.ma.core import equal
+
 import game_rules
 from game_rules import *
 import numpy as np
 
-white_scoring_matrix = np.array([[16.16, -3.03, 0.99, 0.43, 0.63, 1.33, -4.12, 16.16],
-                          [-4.12, -1.81, -0.08, -0.27, -0.18, -0.04, -1.81, -3.03],
-                          [1.33, -0.04, 0.51, 0.07, -0.04, 0.51, -0.08, 0.99],
-                          [0.63, -0.18, -0.04, -0.01, -0.01, 0.07, -0.27, 0.43],
-                          [0.43, -0.27, 0.07, -0.01, -0.01, -0.04, -0.18, 0.63],
-                          [0.99, -0.08, 0.51, -0.04, 0.07, 0.51, -0.04, 1.33],
-                          [-3.03, -1.81, -0.04, -0.18, -0.27, -0.08, -1.81, -4.12],
-                          [16.16, -4.12, 1.33, 0.63, 0.43, 0.99, -3.03, 16.16],
-                  ])
+white_scoring_matrix = np.array([
+    [20.0, -3.5, 1.5, 0.5, 0.5, 1.5, -3.5, 20.0],  # Corners emphasized more
+    [-3.5, -2.0, -0.5, -0.3, -0.3, -0.5, -2.0, -3.5],  # Edge penalties adjusted
+    [1.5, -0.5, 0.7, 0.1, 0.1, 0.7, -0.5, 1.5],  # Inner squares slightly increased
+    [0.5, -0.3, 0.1, 0.0, 0.0, 0.1, -0.3, 0.5],  # Center squares neutral
+    [0.5, -0.3, 0.1, 0.0, 0.0, 0.1, -0.3, 0.5],  # Center squares neutral
+    [1.5, -0.5, 0.7, 0.1, 0.1, 0.7, -0.5, 1.5],  # Inner squares slightly increased
+    [-3.5, -2.0, -0.5, -0.3, -0.3, -0.5, -2.0, -3.5],  # Edge penalties adjusted
+    [20.0, -3.5, 1.5, 0.5, 0.5, 1.5, -3.5, 20.0],  # Corners emphasized more
+])
+
 black_scoring_matrix = white_scoring_matrix.transpose()
 
 
@@ -35,7 +39,7 @@ class AI:
             temp_board = copy.deepcopy(board)
             make_move(temp_board, move[0], move[1], self.player_color)
             evaluation = self.min_player(temp_board, depth - 1, alpha, beta)[0]
-            if evaluation > max_eval:
+            if best_move is None or evaluation > max_eval:
                 max_eval = evaluation
                 best_move = move
 
@@ -43,7 +47,6 @@ class AI:
             if beta <= evaluation:
                 break
         return max_eval, best_move
-
 
     def min_player(self, board, depth, alpha, beta):
         best_move = None
@@ -56,7 +59,7 @@ class AI:
             temp_board = copy.deepcopy(board)
             make_move(temp_board, move[0], move[1], self.opposite_color)
             evaluation = self.max_player(temp_board, depth - 1, alpha, beta)[0]
-            if evaluation < min_eval:
+            if best_move is None or evaluation < min_eval:
                 min_eval = evaluation
                 best_move = move
             beta = min(beta, evaluation)
@@ -65,23 +68,26 @@ class AI:
         return min_eval, best_move
 
     def heuristics(self, board):
-        frontier_weight = -0.5
-        position_weight = 1.0
+        position_weight = 1.2
+        frontier_weight = -0.3
+        mobility_weight = 0.8
 
         white_frontier_score, black_frontier_score = self.get_frontier_disks_score(board)
         white_position_score, black_position_score = self.get_positions_score(board)
+        white_mobility, black_mobility = self.get_mobility_score(board)
         terminal_score = self.get_terminal_score(board)
 
         if self.player_color == CellStates.WHITE:
             frontier_score = frontier_weight * (white_frontier_score - black_frontier_score)
             position_score = position_weight * (white_position_score - black_position_score)
+            mobility_score = mobility_weight * (white_mobility - black_mobility)
         else:
             frontier_score = frontier_weight * (black_frontier_score - white_frontier_score)
             position_score = position_weight * (black_position_score - white_position_score)
+            mobility_score = mobility_weight * (black_mobility - white_mobility)
 
-        return frontier_score + position_score + terminal_score
+        return terminal_score if terminal_score != 0 else frontier_score + position_score + mobility_score
 
-    # Count frontier disks for both players
     def get_frontier_disks_score(self, board):
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
         white_frontier = 0
@@ -97,7 +103,6 @@ class AI:
                         if 0 <= neighbor_i < 8 and 0 <= neighbor_j < 8 and board[neighbor_i][neighbor_j].state == CellStates.EMPTY:
                             is_frontier = True
                             break
-
                     if is_frontier:
                         if board[i][j].state == CellStates.BLACK:
                             black_frontier += 1
@@ -115,6 +120,11 @@ class AI:
                 elif board[i][j].state == CellStates.BLACK:
                     black_score += black_scoring_matrix[i][j]
         return white_score, black_score
+
+    def get_mobility_score(self, board):
+        white_mobility = len(get_valid_plays(board, CellStates.WHITE))
+        black_mobility = len(get_valid_plays(board, CellStates.BLACK))
+        return white_mobility, black_mobility
 
     def get_terminal_score(self, board):
         if game_rules.is_terminal(board):
